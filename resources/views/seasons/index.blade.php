abf0e9c9aa15fd93f90aa6cf6f0902b537218ed9
gomoesalbino <gomesalbino26@outlook.com>
2022-10-24 17:37:18 -0300

    Atualizando o botão de voltar na view temporada

--- resources/views/seasons/index.blade.php
+++ resources/views/seasons/index.blade.php
@@ -1,17 +1,24 @@
 <x-layout title="Temporada: '{!! $series->nome !!}'">
   
-        <ul class="list-group mb-3 mt-3">
-            @foreach ($seasons as $season)
-                <li class="list-group-item d-flex justify-content-between algin-items-center">
-                    
-                     Temporada:  {{ $season->number }}
-               
-                    <span class="badge bg-success">
-                    {{ $season->episodes->count() }}
-                    </span>
+      <div class="mt-3">
+            <ul class="list-group mb-3 mt-3">
+                @foreach ($seasons as $season)
+                    <li class="list-group-item d-flex justify-content-between algin-items-center">
+                        
+                        Temporada:  {{ $season->number }}
+                
+                        <span class="badge bg-success">
+                        {{ $season->episodes->count() }}
+                        </span>
 
-                </li>
-            @endforeach
-        </ul>
-
+                    </li>
+                @endforeach
+                
+            </ul>
+            <div class="mt-3">
+                <a class="btn btn-primary btn-lg ms-3" href="{{ route('series.index') }}">
+                <i class="fa-sharp fa-solid fa-backward-fast"></i>
+                <a/>
+            </div>
+      </div>
 </x-layout>
