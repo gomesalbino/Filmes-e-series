a10df5860f89840457cf62750b617ef6f33187ba
gomoesalbino <gomesalbino26@outlook.com>
2022-10-25 21:14:16 -0300

    Atualizando as rotas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return to_route('login');
-})->middleware(['auth', 'verified'])->name('login');
+})->middleware(Authenticate::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
